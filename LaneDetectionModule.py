b808bf3a7683dbc9efe49d8b7b917bde43cb2515
Safat99 <sarkersafat99@gmail.com>
2021-04-15 16:57:32 +0600

    warping done!

--- LaneDetectionModule.py
+++ LaneDetectionModule.py
@@ -5,19 +5,38 @@ import utils
 
 
 def getLaneCurve(img):
+	#sent an image and get the value of curve
 	
+	######step 1
 	imgThresh = utils.thresholding(img)
-	cv2.imshow('Threshold', imgThresh)
 	
+	######step 2
+	h, w, c = img.shape	
+	#for points we need trackbar
+	points = utils.valTrackbars()
+	imgWarp = utils.warpImg(img, points, w,h)
+	imgWarpPoints = utils.drawPoints(img,points)
+
+	cv2.imshow('Threshold', imgThresh)
+	cv2.imshow('Warp', imgWarp)
+	cv2.imshow('Warp Points', imgWarpPoints)
 	return None
 
 
-
 if __name__ == '__main__':
-	cap = cv2.VideoCapture(0)	
-	while True:
+	cap = cv2.VideoCapture('vid1.mp4')	
+	intialTracbarVals = [102,80,20,214]
+	utils.initalizeTrackbars(intialTracbarVals)
+	frameCounter = 0
+	
+	while True:	
+		frameCounter +=1
+		if cap.get(cv2.CAP_PROP_FRAME_COUNT) == frameCounter:
+			cap.set(cv2.CAP_PROP_POS_FRAMES,0)
+			frameCounter=0
+		
 		_, img = cap.read()
-		img = cv2.resize(img, (640,480))
+		img = cv2.resize(img,(480,240))
 		getLaneCurve(img)
 		
 		cv2.imshow('frame', img)
--- utils.py
+++ utils.py
@@ -3,8 +3,47 @@ import cv2
 
 def thresholding(img):
 	imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	lowerwhite = np.array([0,0,0])
-	upperwhite = np.array([179,255,255])
+	lowerwhite = np.array([88,0,0])
+	upperwhite = np.array([135,255,255])
 	maskwhite = cv2.inRange(imgHSV, lowerwhite, upperwhite)
 	
 	return maskwhite
+	
+
+def warpImg (img,points,w,h,inv=False):
+    pts1 = np.float32(points)
+    pts2 = np.float32([[0,0],[w,0],[0,h],[w,h]])
+    if inv:
+        matrix = cv2.getPerspectiveTransform(pts2,pts1)
+    else:
+        matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    imgWarp = cv2.warpPerspective(img,matrix,(w,h))
+    return imgWarp
+	
+
+def nothing(x):
+	pass	
+
+def initalizeTrackbars(intialTracbarVals, wT=480, hT=240):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0],wT//2, nothing)
+    cv2.createTrackbar("Height Top", "Trackbars", intialTracbarVals[1], hT, nothing)
+    cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2],wT//2, nothing)
+    cv2.createTrackbar("Height Bottom", "Trackbars", intialTracbarVals[3], hT, nothing)
+    
+def valTrackbars(wT=480, hT=240):
+    widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
+    heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
+    widthBottom = cv2.getTrackbarPos("Width Bottom", "Trackbars")
+    heightBottom = cv2.getTrackbarPos("Height Bottom", "Trackbars")
+    points = np.float32([(widthTop, heightTop), (wT-widthTop, heightTop),
+                      (widthBottom , heightBottom ), (wT-widthBottom, heightBottom)])
+    return points
+    
+def drawPoints(img,points):
+    for x in range( 0,4):
+        cv2.circle(img,(int(points[x][0]),int(points[x][1])),15,(0,0,255),cv2.FILLED)
+    return img
+
+

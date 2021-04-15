50f40358a21cb45a14786e3bfc613d1b0a90fdbb
Safat99 <sarkersafat99@gmail.com>
2021-04-15 23:04:49 +0600

    optimizing the curve lesson learned

--- LaneDetectionModule.py
+++ LaneDetectionModule.py
@@ -2,25 +2,73 @@ import numpy as np
 import cv2
 import utils
 
+curveList = []
+avrgVal = 10
 
-
-def getLaneCurve(img):
+def getLaneCurve(img,display=2):
 	#sent an image and get the value of curve
-	
+	imgCopy = img.copy()
+	imgResult = img.copy()
 	######step 1
 	imgThresh = utils.thresholding(img)
 	
 	######step 2
-	h, w, c = img.shape	
+	hT, wT, c = img.shape	
 	#for points we need trackbar
 	points = utils.valTrackbars()
-	imgWarp = utils.warpImg(img, points, w,h)
-	imgWarpPoints = utils.drawPoints(img,points)
+	imgWarp = utils.warpImg(imgThresh, points, wT,hT)
+	imgWarpPoints = utils.drawPoints(imgCopy,points)
 
+
+	#############step 3
+	middlePoint, imgHist = utils.getHistogram(imgWarp,display=True,minPer=0.5,region=4)
+	curveAveragePoint, imgHist = utils.getHistogram(imgWarp,display=True,minPer=0.9)
+	#print(basePoint - midPoint)
+	curveRaw = curveAveragePoint - middlePoint
+
+
+	##########step 4 (avrg)
+	#avrging because want to smooth transition>> noise reduce happens
+	curveList.append(curveRaw)
+	if len(curveList) > avrgVal:
+		curveList.pop(0) # maane koyta niye avrg korbo otar beshi batch hoye gele frst er ta baad diye dibe
+	curve = int(sum(curveList)/len(curveList))
+	
+	
+	#####Step 5  Display
+	if display != 0:
+		   imgInvWarp = utils.warpImg(imgWarp, points, wT, hT,inv = True)
+		   imgInvWarp = cv2.cvtColor(imgInvWarp,cv2.COLOR_GRAY2BGR)
+		   imgInvWarp[0:hT//3,0:wT] = 0,0,0
+		   imgLaneColor = np.zeros_like(img)
+		   imgLaneColor[:] = 0, 255, 0
+		   imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
+		   imgResult = cv2.addWeighted(imgResult,1,imgLaneColor,1,0)
+		   midY = 450
+		   cv2.putText(imgResult,str(curve),(wT//2-80,85),cv2.FONT_HERSHEY_COMPLEX,2,(255,0,255),3)
+		   cv2.line(imgResult,(wT//2,midY),(wT//2+(curve*3),midY),(255,0,255),5)
+		   cv2.line(imgResult, ((wT // 2 + (curve * 3)), midY-25), (wT // 2 + (curve * 3), midY+25), (0, 255, 0), 5)
+		   for x in range(-30, 30):
+		       w = wT // 20
+		       cv2.line(imgResult, (w * x + int(curve//50 ), midY-10),
+		                (w * x + int(curve//50 ), midY+10), (0, 0, 255), 2)
+		   #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+		   #cv2.putText(imgResult, 'FPS '+str(int(fps)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (230,50,50), 3);
+	if display == 2:
+		imgStacked = utils.stackImages(0.7,([img,imgWarpPoints,imgWarp],
+			[imgHist,imgLaneColor,imgResult]))
+		cv2.imshow('ImageStack',imgStacked)
+   
+	elif display == 1:
+		cv2.imshow('Result',imgResult)
+	
+	'''
 	cv2.imshow('Threshold', imgThresh)
 	cv2.imshow('Warp', imgWarp)
 	cv2.imshow('Warp Points', imgWarpPoints)
-	return None
+	cv2.imshow('Histogram', imgHist)
+	'''
+	return curve
 
 
 if __name__ == '__main__':
